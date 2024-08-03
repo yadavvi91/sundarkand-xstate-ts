16cbc314410941d46705e22e7a6fdc56996c32ef
Vishal Yadav <yadavvi91@gmail.com>
2024-08-03 14:26:13 +0530

    This isn't correct but let us commit the previous thing whatever it was

--- src/audioPlayerMachine.ts
+++ src/audioPlayerMachine.ts
@@ -1,4 +1,4 @@
-import { createMachine, assign, ActorRefFrom, StateFrom } from "xstate";
+import { createMachine, assign, ActorRefFrom, StateFrom, setup } from "xstate";
 
 export interface Lyric {
   time: number;
@@ -33,7 +33,7 @@ const updateCurrentTime = assign(({ context, event }) => {
   
   const newCurrentTime = event.currentTime;
   const newLyricIndex = context.lyrics.findIndex(
-    (lyric, index) =>
+    (lyric: Lyric, index: number) =>
       lyric.time <= newCurrentTime &&
       (index === context.lyrics.length - 1 || context.lyrics[index + 1].time > newCurrentTime)
   );
@@ -49,7 +49,7 @@ const seekToTime = assign(({ context, event }) => {
   
   const newTime = event.time;
   const newLyricIndex = context.lyrics.findIndex(
-    (lyric, index) =>
+    (lyric: Lyric, index: number) =>
       lyric.time <= newTime &&
       (index === context.lyrics.length - 1 || context.lyrics[index + 1].time > newTime)
   );
@@ -72,7 +72,49 @@ const setLyrics = assign(({ event }) => {
   return { lyrics: event.type === "SET_LYRICS" ? event.lyrics : undefined };
 });
 
+const feedbackMachine = setup({
+  actions: {
+    updateFeedback: assign({
+      feedback: ({ event }) => {
+        return event.value;
+      }
+    })
+  },
+  guards: {
+    feedbackValid: ({ context }) => {
+      return context.feedback.trim().length > 0;
+    }
+  }
+}).createMachine({
+  context: {
+    feedback: ''
+  },
+  inital: 'prompt',
+  states: {
+    prompt: {
+      on: {
+        'feedback.good': {
+          target: 'thanks'
+        },
+        'feedback.bad': {
+          target: 'form'
+        },
+      }
+    },
+    form: {
+      on: {
+        'feedback.update': {
+          actions: assign(({ event }) => {
+            return event.value
+          })
+        }
+      }
+    }
+  }
+});
+
 export const audioPlayerMachine = createMachine({
+  /** @xstate-layout N4IgpgJg5mDOIC5QEMCuECWB7ACgG2QE8wAnAOjy2UwDsoBiAGQHkBBAEQFF2BtABgC6iUAAcssDABdsNYSAAeiAIwAOAOxkAbAFZNAJgAsATgDM27QYPa+JgDQhCibSoNkTRvSZV6jfTy4BfAPs0TFwCYnJKagw6egBlTgAVAH1GAE0AJQBJAGF4-iEkEDEJaSxZYsUEAz17RwRrTTIDJWczH209TS8gkPRsfCJSMhIwakJ6JOyAWU4UgFUcdlYkzkK5UqkZOWra5qMlNSNNIwNNAxVtVU16xHcNFRNDIzPjtSVLAz6QUMGIkZjCYJTicADSG2KW3KlVAe20JjIZ0+nx6n0OagMdwQekxLW6enMah6KlePR+f3Cw3IQIgkwAasxGAs5ilcgAJVgAOQA4utBJtxNsKrtEK0+FolM91AZnmorudsUcNEY1CYlHoVEobJpVJ8KQMqZFRuM6VNmDyeYx5jMFmtIaIhTDRTU9HoyLi1cYLK95SYsQ5EFqyF0+GG+JYTFHNDGDWEhsbaYQyCIIrEGDhWAtEg6Sk6dlUxRYyEdVJi+NcDGpMbdA41NCoyPLdLj2lHunH-tSTRMU2hYJB6DhGKx0rnoQW4UXXKWZRXPtXFXXdUYPVddWoK5o+NujEFgiAaFgIHA5JSE6RBWVJwpEABaWsNO-aJEqcPvj-fA-ngFRKi0KAr2FWFbwQXUyArK5Dm8f0bBMNRsW3R4GxOTpzCsExOyNQFTQaR1rxFQscVMFpiV0GwjG0VUwwQutLiUNcWw+dxXior9+njX8ezpFM0zoIDnSI3RmmlDUXAsawlCMbFtGrNwUJjc4nmrLCLxpXC+1QAcIAEm9qn9Biqx0bd3Cozc+FohpZNcFQUOuK5nAuPR9wCIA */
   id: "audioPlayer",
   types: {} as {
     context: AudioPlayerContext;
