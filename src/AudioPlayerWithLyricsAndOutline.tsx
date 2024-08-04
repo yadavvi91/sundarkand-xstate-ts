723866f97f136e36ecda55a353925f120798b3cc
Vishal Yadav <yadavvi91@gmail.com>
2024-08-04 12:59:46 +0530

    this does not work

--- src/AudioPlayerWithLyricsAndOutline.tsx
+++ src/AudioPlayerWithLyricsAndOutline.tsx
@@ -14,8 +14,14 @@ import hanumanji from "./assets/hanumanji.jpg";
 import { outline } from "./utils/lyrics.ts";
 
 const AudioPlayerWithLyricsAndOutline: React.FC = () => {
-  const [state, send] = useMachine(audioPlayerMachine);
-
+  const [state, send] = useMachine(audioPlayerMachine, {
+    setup: ({ context }) => ({
+      context: {
+        ...context,
+        outline: outline,
+      },
+    }),
+  });
   const audioRef = useRef<HTMLAudioElement>(null);
   const progressRef = useRef<HTMLDivElement>(null);
   const lyricsContainerRef = useRef<HTMLDivElement>(null);
